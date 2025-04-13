644c3524eb0dbf53db29814489743d27f6b643fd
Anand <anandraj6783@gmail.com>
2025-04-13 12:25:33 +0530

    cahnged the function for course creation

--- course.h
+++ course.h
@@ -1,15 +1,18 @@
 #ifndef COURSE_H
 #define COURSE_H
 
-#include "student.H" // include the student file for the student datatype.
+#include "student.c" // include the student file for the student datatype.
 
 typedef struct course {
 
     char courseName[50];
+    int passingGrades;
     double averageGrades;
-    int totalStudents;
-    Student *students;
+    int totalStudentsInCourse;
+    Student *studentsInCourse;
 
 } Course;
+
+Course *courseCreation ();
 
 #endif
